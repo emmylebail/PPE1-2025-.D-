74fa47b3c1009a223abab1b67a33ce74aa88769b
Emmy LE BAIL <emmylebail@MacBook-Air-de-Emmy.local>
2025-10-12 23:44:08 +0200

    Nouvel enregistrement

--- Devoirs/Exercice1/ann/comptes.sh
+++ Devoirs/Exercice1/ann/comptes.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
-echo "Nombre de Lieu en 2016 : "
-cat 2016/*/* | grep Location | wc -l > output.txt
-echo "Nombre de Lieux en 2017 : "
-cat 2017/*/* | grep Location | wc -l > output.txt
-echo "Nombre de Lieux en 2018 : "
-cat 2018/*/* | grep Location | wc -l > output.txt
+echo "Nombre de Lieux en 2016 :"
+grep "Location" 2016/*/*.ann | wc -l
+
+echo "Nombre de Lieux en 2017 :"
+grep "Location" 2017/*/*.ann | wc -l
+
+echo "Nombre de Lieux en 2018 :"
+grep "Location" 2018/*/*.ann | wc -l
